e9079dea2f3de2f3391918a0a24ab7f683c8481f
Phan Bình Nguyên Lâm <76941117+LamTerQH@users.noreply.github.com>
2021-12-11 16:11:52 +0700

    Update fence.cpp

--- blogs/Binary_Search/fence.cpp
+++ blogs/Binary_Search/fence.cpp
@@ -4,14 +4,16 @@ using namespace std;
 
 using lli = long long int;
 
-const int N   = 1e4+7;
-const lli INF = 1e11+7;
+const int N   = 1e4 + 7;
+const lli INF = 1e11 + 7;
 
 lli n,k;
 lli num[N];
 
 bool query(lli x) {
-    //Tự code
+    lli res = 0;
+    for(int i = 1; i <= n; i++) res += num[i] / x;
+    return res >= k;
 }
 
 #define TASK "fence"
@@ -19,24 +21,24 @@ bool query(lli x) {
 int main() {
 
     //Mở file
-    if(fopen(TASK".inp", "r")) {
+    if (fopen(TASK".inp", "r")) {
         freopen(TASK".inp", "r", stdin);
         freopen(TASK".out", "w", stdout);
     }
 
     cin >> n >> k;
-    for(int i = 1; i <= n; i++) cin >> num[i], num[i] *= 100;
+    for (int i = 1; i <= n; i++) cin >> num[i], num[i] *= 100;
     //nhân num[i] lên 100 lần để tránh việc xử lý số chấm động
 
     //Giới hạn trên, giới hạn dưới và đáp án
     lli L = 1, R = INF, res = 0;
 
-    while(L <= R) {
+    while (L <= R) {
         //Lấy phần tử ở giữa
         lli mid = (L + R) >> 1;
 
         //Kiểm tra "lớn hơn", "bé hơn"
-        if(query(mid)) L = mid + 1, res = mid; //Tăng giới hạn và cập nhật lại kết quả
+        if (query(mid)) L = mid + 1, res = mid; //Tăng giới hạn và cập nhật lại kết quả
         else R = mid - 1; //Giảm giới hạn
     }
 
